3cea5be7d181bbfe0683e49810043d756d468e7a
João Lucas <lucas.xms@gmail.com>
2025-09-05 21:11:25 -0300

    feature: script

---
+++ app.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv(
+    "data/Final Dataset - State of Data 2024 - Kaggle - df_survey_2024.csv",
+    sep=",",
+    encoding="utf-8",
+)
+
+print(df.head())
+print(df.head())
